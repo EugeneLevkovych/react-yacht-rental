605a4682b32cec08972757542f43e546360e1050
Eugene <eugenelevkovych@yahoo.com>
2025-06-16 11:18:24 +0300

    add hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ name, href }) {
     <li>
       <a
         href={href}
-        className="flex justify-center items-center w-22.5 h-10 rounded-lg bg-turquoise-blue2 hover:bg-turquoise-blue1"
+        className="flex justify-center items-center w-22.5 h-10 rounded-lg bg-turquoise-blue2 hover:bg-turquoise-blue1 transition-all"
       >
         {name}
       </a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,17 @@ export default function Hero() {
         <h1 className="font-medium text-[2rem] md:text-[4.38rem] xl:text-[5rem] my-let-spacing2 text-center leading-none text-white1 mb-6">
           Explore the High Seas with Our Luxury Yacht Rentals
         </h1>
-        <p className="font-light text-xs md:text-base leading-[1.16] md:leading-[1.4] text-white2 text-center md:px-13.5 mb-9.5 md:mb-12">
+        <p className="font-light text-xs md:text-base leading-[1.16] md:leading-[1.4] text-white2 text-center md:px-13.5 xl:px-50.5 mb-9.5 md:mb-12">
           Experience the freedom and luxury of yachting on the open seas. Choose
           from our top-of-the-line yachts and embark on an unforgettable journey
           surrounded by breathtaking views and state-of-the-art amenities.
         </p>
+        <a
+          className="max-w-45 block bg-turquoise-blue1 hover:bg-white1 font-medium text-lg leading-[1.1] my-let-spacing2 text-white1 hover:text-turquoise-blue1 transition-all rounded-2xl px-10 py-4 mx-auto"
+          href="#"
+        >
+          Yacht rental
+        </a>
       </div>
     </div>
   );
