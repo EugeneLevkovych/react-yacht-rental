e19ee003d10d8ca1daa119759b7997fc6f701e78
Eugene <eugenelevkovych@yahoo.com>
2025-06-18 21:10:55 +0300

    add md footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ import SvgSocials from './SvgSocials';
 export default function Footer() {
   return (
     <div className="container mx-auto px-4">
-      <div className="text-white1 bg-turquoise-blue1 rounded-t-3xl pt-10 md:pt-12.5 pb-2 px-7.5 md:px-12">
-        <div className="flex flex-col gap-20">
+      <div className="text-white1 bg-turquoise-blue1 rounded-t-3xl pt-10 md:pt-12.5 pb-5 px-7.5 md:px-12">
+        <div className="flex flex-col md:flex-row gap-20 md:gap-22.5 mb-10 md:mb-24">
           <div className="flex justify-between">
             <a
               className="font-medium text-sm my-let-spacing1 text-white1"
@@ -36,6 +36,16 @@ export default function Footer() {
               ))}
             </ul>
           </div>
+        </div>
+        <div className="max-w-69.5 md:max-w-160 xl:max-w-173 mb-10.5 md:mb-24.5 mx-auto">
+          <p className="text-lg md:text-[2rem] leading-none my-let-spacing2 text-center">
+            No matter which yacht you choose, our experienced crew will ensure
+            that your journey is smooth and enjoyable.
+          </p>
+        </div>
+        <div className="flex justify-between   max-w-62 w-full text-xs leading-[1.16] tracking-[0.03em] text-center mx-auto">
+          <p>2023. Yacht Adventures</p>
+          <p>Privacy</p>
         </div>
       </div>
     </div>
